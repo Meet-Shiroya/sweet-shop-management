7f2fbc3924c15596376343b3fe916250744765cf
Meet-Shiroya <meetshiroya867@gmail.com>
2025-07-17 21:07:23 +0530

    Search functionality

--- sweetshop.py
+++ sweetshop.py
@@ -23,13 +23,29 @@ class SweetShop:
         for sweet in self.sweets:
             print(sweet)
 
+    def search_sweets(self, keyword):
+        print(f"\n🔍 Search results for '{keyword}':")
+        found = False
+        for sweet in self.sweets:
+            if keyword.lower() in sweet.name.lower() or keyword.lower() in sweet.category.lower():
+                print(sweet)
+                found = True
+        if not found:
+            print("❌ No sweets found with that keyword.")
+
 
 # Demo
 if __name__ == "__main__":
     shop = SweetShop()
     sweet1 = Sweet(1001, "Kaju Katli", "Nut-Based", 50, 20)
     sweet2 = Sweet(1002, "Gulab Jamun", "Milk-Based", 30, 50)
+    sweet3 = Sweet(1003, "Gajar Halwa", "Vegetable-Based", 40, 25)
 
     shop.add_sweet(sweet1)
     shop.add_sweet(sweet2)
+    shop.add_sweet(sweet3)
+
     shop.view_sweets()
+    shop.search_sweets("Milk")
+    shop.search_sweets("Halwa")
+    shop.search_sweets("Candy")
